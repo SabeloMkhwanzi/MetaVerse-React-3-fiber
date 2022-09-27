5f6af881b2c259f400dbab7bb8b4b9dcd6a30b9c
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-09-27 19:50:39 +0200

    commit project set + foundation

---
+++ components/AnimatedBox.tsx
@@ -0,0 +1,21 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const AnimatedBox = () => {
+  const meshRef = useRef<THREE.Mesh>(null);
+  useFrame(() => {
+    console.log("hi");
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.1;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} scale={[0.5, 0.5, 0.5]}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
+};
+
+export default AnimatedBox;
