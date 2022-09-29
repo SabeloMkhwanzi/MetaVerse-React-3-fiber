4869345ffd12dd46eaa83650510ad6a0cd78037e
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-09-29 19:31:27 +0200

    added gride + helper

--- components/AnimatedBox.tsx
+++ components/AnimatedBox.tsx
@@ -1,8 +1,18 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { useHelper } from "@react-three/drei";
+import { BoxHelper } from "three";
 
-const AnimatedBox = () => {
+type Props = {
+  isTesting: boolean;
+};
+
+const AnimatedBox: React.FC<Props> = ({ isTesting }) => {
   const meshRef = useRef<THREE.Mesh>(null);
+  {
+    isTesting ? useHelper(meshRef, BoxHelper, "blue") : null;
+  }
+
   useFrame(() => {
     console.log("hi");
     if (meshRef.current) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
-import CameraOrbitController from "../components/CameraOrbitController";
+import { OrbitControls, Stats } from "@react-three/drei";
 
 const Home: NextPage = () => {
+  const testing = true;
+
   return (
     <div className="container">
       <Canvas>
-        <CameraOrbitController />
+        {testing ? <Stats /> : null}
+        {testing ? <OrbitControls /> : null}
+        {testing ? <gridHelper args={[10, 10]} /> : null}
+        <axesHelper visible={testing} args={[2]} />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <AnimatedBox />
+        <AnimatedBox isTesting={testing} />
       </Canvas>
     </div>
   );
