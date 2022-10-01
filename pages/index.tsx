b697d89eaa77dd437aa6eff3518e6f3a2d4ba7c1
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-10-01 20:23:27 +0200

    added lights  + ground components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,22 @@
 import type { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stats, useTexture } from "@react-three/drei";
+import Lights from "../components/Ligths";
+import Ground from "../components/Ground";
 
 const TexturedSphere = () => {
   const map = useTexture("./textures/metal_plate_diff_1k.png");
-  const displacementMap = useTexture("./textures/metal_plate_disp_1k.png");
   const normalMap = useTexture("./textures/metal_plate_nor_gl_1k.png");
   const roughnessMap = useTexture("./textures/metal_plate_rough_1k.png");
 
   return (
     <>
-      <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial map={map} />
-      </mesh>
-      <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[0, 1, 0]} castShadow>
         <sphereGeometry />
         <meshStandardMaterial
           map={map}
           normalMap={normalMap}
           roughnessMap={roughnessMap}
-        />
-      </mesh>
-      <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
-        <sphereGeometry args={[1, 200, 200]} />
-        <meshStandardMaterial
-          map={map}
-          displacementMap={displacementMap}
-          normalMap={normalMap}
-          roughnessMap={roughnessMap}
-          displacementScale={0.05}
         />
       </mesh>
     </>
@@ -41,14 +28,14 @@ const Home: NextPage = () => {
 
   return (
     <div className="container">
-      <Canvas>
+      <Canvas shadows>
         {testing ? <Stats /> : null}
-        {testing ? <OrbitControls /> : null}
+        {testing ? <axesHelper args={[2]} /> : null}
         {testing ? <gridHelper args={[10, 10]} /> : null}
-        <axesHelper visible={testing} args={[2]} />
-        <ambientLight intensity={0.3} />
-        <directionalLight position={[0, 5, 5]} />
+        <OrbitControls />
         <TexturedSphere />
+        <Lights />
+        <Ground />
       </Canvas>
     </div>
   );
